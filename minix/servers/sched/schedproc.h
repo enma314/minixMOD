b58d099ab796cd427e84546fb4718efa2bc69760
JasonParra <jasonparra96@gmail.com>
2017-04-07 13:14:44 -0400

    errors

--- minix/servers/sched/schedproc.h
+++ minix/servers/sched/schedproc.h
@@ -33,6 +33,7 @@ EXTERN struct schedproc {
 	bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)]; /* what CPUs is the
 								process allowed
 								to run on */
+	unsigned nice;
 	unsigned ticketsNum;
 
 } schedproc[NR_PROCS];
