edcc78bdb46e5d4b8e8f84f264605221881d58f6
JasonParra <jasonparra96@gmail.com>
2017-04-10 13:51:21 -0400

    fixing

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -459,7 +459,7 @@ static void balance_queues(minix_timer_t *tp)
  	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
  		if ((rmp->flags & IN_USE) && PROCESS_IN_USER_Q(rmp)) {
  			if (USER_Q == rmp->priority)
-			
+
 				procesos++;
 
  		}
